3823169374c4f12183fd2a66d9af4f88d8a86560
CoDeFi <cimran@gmail.com>
2021-09-09 11:15:13 +0100

    Pools Revamping - WIP

--- src/components/List/Pools.vue
+++ src/components/List/Pools.vue
@@ -6,37 +6,89 @@
     <Container v-if="title" class="d-flex flex-items-center px-4 px-md-0 mb-3">
       <h3 class="flex-auto" v-text="title" />
     </Container>
-    <!-- <b-card-group deck> -->
-    <vue-justified-layout
-      :items="pools"
-      v-slot="{ item }"
-      :options="{
-        forceAspectRatio: 1,
-        boxSpacing: 25
-      }"
-    >
-    {{ item.liquidity }}
-      {{ item.id }}
-      {{ item.swapFee }}
-      {{ item.tokens }}
-      <!-- <b-card
-          :key="i"
-          :pool="pool"
-          :bg-variant="grey"
-          text-variant="white"
-          style="width: 5rem; height: 150px; border: solid red 1px; margin:10px"
-          class="text-center"
-        >
-          <b-card-text>{{ pool.id }} Width comes here</b-card-text>
-        </b-card> -->
-    </vue-justified-layout>
-    <!-- </b-card-group> -->
+    <!-- infinite scroll -->
+    <div v-infinite-scroll="loadMore" infinite-scroll-distance="2">
+      <div v-if="pools.length > 0">
+        <vue-justified-layout
+          :items="pools"
+          v-slot="{ item }"
+          :options="{
+            forceAspectRatio: 1,
+            containerPadding: 5,
+            boxSpacing: 5
+          }"
+          ><div class="border highlight anim-fade-in rounded-1">
+            <a :href="'#/pool/' + item.id" class="myForm">
+              <div id="contact-details">
+                <span
+                  v-text="$t('marketCap')"
+                  class="text-right text-white strong"
+                />:
+                <span v-text="_num(getLiquidity(item), 'usd-long')" />
+                <div style="margin-top: 10px;" class="grouptext">
+                  <span v-text="$t('swapFee')" class="row text-white" />
+                  <span>: <UiNum :value="item.swapFee" format="percent"/></span>
+                </div>
+                <div class="grouptext margin-top10">
+                  <span v-text="$t('apy')" class="text-white" />:
+                  <UiNum
+                    :value="item.apy"
+                    format="percent"
+                    class="column hide-sm hide-md"
+                  />
+                </div>
+                <div class="grouptext margin-top10">
+                  <span v-text="$t('volume24')" class="text-white" />:
+                  <span
+                    v-text="_num(item.lastSwapVolume, 'usd-long')"
+                    format="currency"
+                    class=""
+                  />
+                </div>
+                <div class="grouptext margin-top10">
+                  <span v-text="$t('rewardApy')" class="text-white" />:
+                  <UiNum :value="item.rewardApy" format="percent" />
+                </div>
+              </div>
+              <div id="comment-box">
+                <Pie :tokens="item.tokens" class="mr-3" size="60" />
+                <div
+                  v-for="token in item.tokens"
+                  :key="token.address"
+                  :class="token.symbol.length > 14 && 'tooltipped tooltipped-n'"
+                  :aria-label="token.symbol"
+                  class="d-flex flex-items-center mr-2"
+                >
+                  <Icon
+                    name="bullet"
+                    size="12"
+                    :style="`color: ${token.color}`"
+                  />
+                  {{ _num(token.weightPercent / 100, 'percent-short') }}
+                  {{ _shorten(token.symbol, 14) }}
+                </div>
+                <div style="margin-top: 15px;" class="grouptext">
+                  {{ _shortenAddress(item.id) }}
+                </div>
+              </div>
+              <button>View Details</button>
+              <div>
+                <div v-text="$t('myLiquidity')" class="text-white text-left" />
+                <div
+                  v-text="_num(myLiquidity(item), 'usd-long')"
+                  format="currency"
+                  class="column hide-sm hide-md hide-lg"
+                />
+              </div>
+            </a>
+          </div>
+        </vue-justified-layout>
+      </div>
+    </div>
+    <!-- infinite scroll ends -->
     <UiTable>
       <UiTableTh>
-        <div
-          v-text="$t('poolAddress')"
-          class="column-sm text-left hide-sm hide-md hide-lg"
-        />
+        <div v-text="$t('poolAddress')" />
         <div v-text="$t('assets')" class="flex-auto text-left" />
         <div v-text="$t('marketCap')" class="column" />
         <div v-text="$t('swapFee')" class="column hide-sm hide-md" />
@@ -77,6 +129,7 @@
 <script>
 import { mapActions } from 'vuex';
 import { formatFilters, ITEMS_PER_PAGE } from '@/helpers/utils';
+import { getPoolLiquidity } from '@/helpers/price';
 
 export default {
   props: ['query', 'title'],
@@ -107,6 +160,16 @@ export default {
     }
   },
   methods: {
+    getLiquidity(pool) {
+      console.log(pool);
+      return getPoolLiquidity(pool, this.price.values);
+      // return '1212';
+    },
+    myLiquidity(pool) {
+      const poolShares = this.subgraph.poolShares[pool.id];
+      if (!pool.finalized || !poolShares) return 0;
+      return (this.getLiquidity(pool) / pool.totalShares) * poolShares;
+    },
     ...mapActions(['getPools']),
     async loadMore() {
       if (this.pools.length < this.page * ITEMS_PER_PAGE) return;
@@ -124,16 +187,62 @@ export default {
 </script>
 <style scoped>
 .justified-container {
-  background-color: rgb(156, 94, 94);
+  height: auto;
+  background-color: rgba(83, 91, 92, 0.356);
   width: 100%;
 }
-
-.justified-item {
-  background-color: #26a69a;
-  border: 1px orange;
-  /* nth-child(odd)
-    background-color: #26a69a;
-  &:nth-child(even)
-    background-color: #81c784; */
+.margin-top10 {
+  margin-top: 10px;
+}
+* {
+  box-sizing: border-box;
+}
+.myForm {
+  display: grid;
+  grid-template-areas:
+    'comments contact'
+    '... button';
+  grid-template-rows: 12em 2em;
+  grid-template-columns: 8em 1fr;
+  grid-gap: 0.8em;
+  background: linear-gradient(
+    178deg,
+    rgb(56 74 255 / 4%) 23.98%,
+    #253743a1 100%
+  );
+  padding: 1.2em;
+}
+.myForm label {
+  grid-area: labels;
+}
+.myForm input {
+  grid-area: contact;
+  width: 100%;
+  padding: 1.1em;
+  border: none;
+  margin-bottom: 1em;
+}
+.myForm textarea {
+  min-height: calc(100% - 2em);
+  width: 100%;
+  border: none;
+}
+#contact-details {
+  grid-area: contact;
+}
+#comment-box {
+  grid-area: comments;
+}
+.myForm button {
+  grid-area: button;
+  border: 0;
+  background: gray;
+  color: white;
+}
+:root {
+  --bg-shape-color: linear-gradient(120deg, #343a4f, #0f1620);
+  --lightblue: #3d9dea;
+  --darkblue: #4a4eee;
+  --text-color: #d5e1ef;
 }
 </style>
