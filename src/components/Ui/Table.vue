b59b3d46ee3e1ea7c66f1880b3db2c4bbeb86125
launchdex <coldenrun@gmail.com>
2021-11-29 22:54:26 -0500

    Update table style

--- src/components/Ui/Table.vue
+++ src/components/Ui/Table.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="border-left-0 border-right-0 border-md border rounded-0 rounded-md-1 panel-background"
+    class="rounded-0 rounded-md-1 panel-background"
   >
     <slot />
   </div>
